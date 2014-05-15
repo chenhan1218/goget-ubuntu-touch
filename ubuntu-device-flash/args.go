174ff60b2feac0906ef44d287714e3cccd166dea
Jani Monoses <jani@ubuntu.com>
2014-05-15 16:21:24 +0300

    Make help line for RunScript more descriptive

--- ubuntu-device-flash/args.go
+++ ubuntu-device-flash/args.go
@@ -35,7 +35,7 @@ type arguments struct {
 	Server        string `long:"server" description:"Use a different image server"`
 	CleanCache    bool   `long:"clean-cache" description:"Cleans up cache with all downloaded bits"`
 	TLSSkipVerify bool   `long:"tls-skip-verify" description:"Skip TLS certificate validation"`
-	RunScript     string `long:"run-script" description:"Run a script given by path to finish the flashing process, instead of rebooting to recovery"`
+	RunScript     string `long:"run-script" description:"Run a script given by path to finish the flashing process, instead of rebooting to recovery (mostly used during development to work around quirky or incomplete recovery images)"`
 }
 
 var args arguments
