ab8b1ee63042efaf7dbff8b32beeb03ec8e97336
Sergio Schvezov <sergio.schvezov@canonical.com>
2014-07-08 16:24:20 -0300

    Fixing license text s/mms/ubuntu-emulator/

--- ubuntu-emulator/run_test.go
+++ ubuntu-emulator/run_test.go
@@ -6,11 +6,11 @@
  *
  * This file is part of ubuntu-emulator.
  *
- * mms is free software; you can redistribute it and/or modify
+ * ubuntu-emulator is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
  *
- * mms is distributed in the hope that it will be useful,
+ * ubuntu-emulator is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
