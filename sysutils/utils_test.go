0aa368fad2836522aa1ae2654219d9cca2d6e6ab
Sergio Schvezov <sergio.schvezov@canonical.com>
2015-03-18 00:27:36 -0300

    Making the total size 97.5% of the real total size for core and GB sized disks (not GiB)

---
+++ sysutils/utils_test.go
@@ -0,0 +1,50 @@
+package sysutils
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	. "launchpad.net/gocheck"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type UtilsTestSuite struct {
+	tmpdir string
+}
+
+var _ = Suite(&UtilsTestSuite{})
+
+func (s *UtilsTestSuite) SetUpTest(c *C) {
+	s.tmpdir = c.MkDir()
+}
+
+func (s *UtilsTestSuite) TestCreateEmptyFileGiB(c *C) {
+	f := filepath.Join(s.tmpdir, "gib")
+	c.Assert(CreateEmptyFile(f, 1, GiB), IsNil)
+
+	fStat, err := os.Stat(f)
+	c.Assert(err, IsNil)
+
+	c.Assert(fStat.Size(), Equals, int64(1024*1024*1024))
+}
+
+func (s *UtilsTestSuite) TestCreateEmptyFileGB(c *C) {
+	f1 := filepath.Join(s.tmpdir, "gb")
+	c.Assert(CreateEmptyFile(f1, 1, GB), IsNil)
+
+	fStat1, err := os.Stat(f1)
+	c.Assert(err, IsNil)
+
+	c.Assert(fStat1.Size(), Equals, int64(1000*1000*975))
+
+	f2 := filepath.Join(s.tmpdir, "gb")
+	c.Assert(CreateEmptyFile(f2, 2, GB), IsNil)
+
+	fStat2, err := os.Stat(f2)
+	c.Assert(err, IsNil)
+
+	c.Assert(fStat2.Size(), Equals, int64(2*1000*1000*975))
+}
