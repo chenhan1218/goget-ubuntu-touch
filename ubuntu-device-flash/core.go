418a736d1236e3fc973b69aa9052a2ff3162f03d
Sergio Schvezov <sergio.schvezov@canonical.com>
2015-04-23 00:40:37 -0300

    Making stable the default channel

--- ubuntu-device-flash/core.go
+++ ubuntu-device-flash/core.go
@@ -53,7 +53,7 @@ func init() {
 }
 
 type CoreCmd struct {
-	Channel string `long:"channel" description:"Specify the channel to use" default:"edge"`
+	Channel string `long:"channel" description:"Specify the channel to use" default:"stable"`
 	Output  string `long:"output" short:"o" description:"Name of the image file to create" required:"true"`
 	Size    int64  `long:"size" short:"s" description:"Size of image file to create in GB (min 4)" default:"4"`
 	Oem     string `long:"oem" description:"The snappy oem package to base the image out of" default:"generic-amd64"`
